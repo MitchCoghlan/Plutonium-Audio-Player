603a0210f23bd92a29168965f3276183263fc7bb
Mitch Coghlan <127176519+MitchCoghlan@users.noreply.github.com>
2023-07-11 16:17:30 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+   name='plutonium',
+   description='Python Based Audio Player',
+   version='0.0.1',
+   author='Mitch Coghlan',
+   python_requires=">=3.10",
+   install_requires=['pygame==2.5.0']
+) 
